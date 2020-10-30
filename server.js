bb3e83e5fa1aa69621a99cfa21ddf3dff354723f
pierre8237 <34560968+pierre8237@users.noreply.github.com>
2020-10-30 06:46:25 -0400

    why not?

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false
+  useCreateIndex: true
 });
 
 // routes
