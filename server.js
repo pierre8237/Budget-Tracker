9c342533ae4275baf2fdb4c1b198eaa2c9d840fa
pierre8237 <34560968+pierre8237@users.noreply.github.com>
2020-10-30 05:57:14 -0400

    no message

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
   useFindAndModify: false
 });
 
