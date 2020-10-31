135cc3462fc879e056b0aa9dadcfe88cb1ee7d2f
pierre8237 <34560968+pierre8237@users.noreply.github.com>
2020-10-31 09:38:38 -0400

    keeps opening on port 3000 while Im trying to build a react portfolio app. No sleep for at least 48 hrs. You got to be kidding me?

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 2000;
+// const PORT = process.env.PORT || 2000;
 
 const app = express();
 
@@ -15,11 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
